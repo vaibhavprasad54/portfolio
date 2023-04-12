f86085f5c8ddfee8683709b75ebd4bb6b228b3ae
Vaibhav Prasad <vaibhavprasad54@gmail.com>
2023-04-12 14:59:35 +0530

    Resolved download Resume button issue

--- components/About.jsx
+++ components/About.jsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { FaDownload } from "react-icons/fa";
-import javascript from "../public/assets/javascript.png"
-import react from "../public/assets/react.png" 
-import nextjs from "../public/assets/nextjs.png" 
-import github from "../public/assets/github1.png" 
-import firebase from "../public/assets/firebase.png" 
-import mongo from "../public/assets/mongo.png" 
-import node from "../public/assets/node.png" 
-import tailwind from "../public/assets/tailwind.png" 
+import javascript from "../public/assets/javascript.png";
+import react from "../public/assets/react.png";
+import nextjs from "../public/assets/nextjs.png";
+import github from "../public/assets/github1.png";
+import firebase from "../public/assets/firebase.png";
+import mongo from "../public/assets/mongo.png";
+import node from "../public/assets/node.png";
+import tailwind from "../public/assets/tailwind.png";
 
 const About = () => {
   return (
@@ -21,14 +21,17 @@ const About = () => {
         <div className="col-span-2 p-7 sm:p-2">
           <h2>About</h2>
           <p className="py-4 text-gray-600 text-sm sm:text-base">
-            As a frontend developer having experience of 1+ year, skilled in Javascript, Reactjs, Nextjs, and
-            Tailwind, I carry a deep passion for coding and an unwavering
-            commitment to creating robust websites and web-apps. I approach each project with
-            an eagerness to learn, grow, and refine my skills.
-            I also have working knowledge on backend stack such as Node and Express and MongoDB.
-            With a desire to create beautiful and fast user interfaces, I keep learning daily and work on new projects, because as we know, ultimately practice is the key.
-              I am keen to dive deeper into how the universe works and the world around us in space.
-            I also have a Youtube channel where I upload my music covers since last 12 years.
+            As a frontend developer having experience of 1+ year, skilled in
+            Javascript, Reactjs, Nextjs, and Tailwind, I carry a deep passion
+            for coding and an unwavering commitment to creating robust websites
+            and web-apps. I approach each project with an eagerness to learn,
+            grow, and refine my skills. I also have working knowledge on backend
+            stack such as Node and Express and MongoDB. With a desire to create
+            beautiful and fast user interfaces, I keep learning daily and work
+            on new projects, because as we know, ultimately practice is the key.
+            I am keen to dive deeper into how the universe works and the world
+            around us in space. I also have a Youtube channel where I upload my
+            music covers since last 12 years.
           </p>
 
           <div className="">
@@ -93,12 +96,14 @@ const About = () => {
             </div>
           </div>
 
-          <a href="/../public/assets/resumeVaibhav.pdf" download>
-            <button className="p-3 text-base sm:text-lg px-4 flex items-center justify-center bg-blue-800 hover:bg-blue-900 rounded-[4px]">
+          <Link href="../assets/resumeVaibhav.pdf" locale={false}>
+            <button
+              className="p-3 text-base sm:text-lg px-4 flex items-center justify-center bg-blue-800 hover:bg-blue-900 rounded-[4px]">
               Resume
               <FaDownload className="ml-2" />
             </button>
-          </a>
+          </Link>
+
         </div>
         <div className="p-2 px-7 sm:px-2">
           <img
